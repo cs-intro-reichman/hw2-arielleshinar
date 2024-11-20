d06fc3a654e0d513256a01375183d83b41f6b3ff
Arielle Shinar <arielleshinar@Arielles-MacBook-Air.local>
2024-11-20 13:31:12 +0200

    HW2

--- CalcPi.java
+++ CalcPi.java
@@ -6,33 +6,29 @@ public class CalcPi {
 		int num = Integer.parseInt(args[0]);
 		
 		double sum = 0;
-		double n = 1;
+		double n = 1.0 ;
 		//create an if loop that makes sure the input ist zero 
 		
 			//create a for loop that checks how big the number inputted is and adds
-			// or subtracts to the calculation according to wether the i count is even or odd
+			// or subtracts to the calculation 
 
 			for (int i = 0; i <= num; i++){
 				
-				if (i % 2 == 1){
-					sum += ((-1)*(1/(n)));	
-					
-				}
-				else {
-				sum += (1/(n));	
+				
+					sum += (n/((2*i)+1));	
+					n *= -1;
 				
 				}
-				n +=2;
-			}
+			
 		//print the following:
 		//pi according to Java: 3.141592653589793
        // pi, approximated:     result
-	   double result = (sum*4.0) ;
+	   double result = (sum *4.0) ;
 	    
-		System.out.println("pi  according to Java: " + Math.PI);
-		System.out.println("pi,  approximated:     " + result); 
-		
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + result); 
+			}
 	}
-	}
+	
 
 
