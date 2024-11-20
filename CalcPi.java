060b1e5dd9c0adeb105bcebb656d98ba3dbe8b40
Arielle Shinar <arielleshinar@Arielles-MacBook-Air.local>
2024-11-20 14:05:06 +0200

    HW2

--- CalcPi.java
+++ CalcPi.java
@@ -6,13 +6,13 @@ public class CalcPi {
 		int num = Integer.parseInt(args[0]);
 		
 		double sum = 0;
-		double divisor = 1.0 ;
+		double divisor = 1 ;
 		//create an if loop that makes sure the input ist zero 
 		
 			//create a for loop that checks how big the number inputted is and adds
 			// or subtracts to the calculation 
 
-			for (int i = 0; i <= num; i++){
+			for (int i = 0; i < num; i++){
 				
 					double calc = divisor/((2*i)+1);	
 					sum += calc;	
