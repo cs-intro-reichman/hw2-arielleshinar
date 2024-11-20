3145207d1d484c6eec3f473f1ab7306c4a869034
Arielle Shinar <arielleshinar@Arielles-MacBook-Air.local>
2024-11-20 12:32:08 +0200

    HW2

--- CalcPi.java
+++ CalcPi.java
@@ -4,39 +4,35 @@ public class CalcPi {
 	    // create a veriable that will get an input
 
 		int num = Integer.parseInt(args[0]);
-		double divider = 3.0;	
-		double calc = 1.0;
+		
+		double sum = 0;
+		double n = 1;
 		//create an if loop that makes sure the input ist zero 
-			if (num == 0){
-
-				System.out.println("pi according to Java: " + Math.PI);
-				System.out.println("pi, approximated:     " + 0); 
-			}
-			else {
+		
 			//create a for loop that checks how big the number inputted is and adds
 			// or subtracts to the calculation according to wether the i count is even or odd
-			for (int i = 1; i <= num; i++){
-				
-				if (i % 2 != 0){
-					calc -= (1/divider);	
 
+			for (int i = 0; i <= num; i++){
+				
+				if (i % 2 == 1){
+					sum += ((-1)*(1/(n)));	
+					
 				}
 				else {
-				calc += (1/divider);	
+				sum += (1/(n));	
 				
 				}
-				divider += 2 ;
-					
+				n +=2;
 			}
 		//print the following:
 		//pi according to Java: 3.141592653589793
        // pi, approximated:     result
-	   double result = (calc*4.0) ;
+	   double result = (sum*4.0) ;
 	    
 		System.out.println("pi according to Java: " + Math.PI);
 		System.out.println("pi, approximated:     " + result); 
 		
 	}
 	}
-}
+
 
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,65 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+
+		    // Start by writing a loop that prints one hailstone sequence only, for a given seed value (N)
+			//nest this loop inside an outer loop that varies the seed from 1 to N
+			
+			int n = Integer.parseInt(args[0]);
+			int counter = 0 ;
+			int newN = 0 ;
+			int count1 = 0;
+
+		//create a for loop that starts with the number 1 and goes over the numbers of n
+		//and prints the steps until they reach 1
+		//create a seperate condition for the time n == 1 since it will not enter the following while
+		//condition that multiplies and divides the number according to the rules until the number reaches 1 again
+		
+		for (int i=1; i <= n; i++){
+			
+			
+			counter = 0 ;
+			
+			if (i == 1) {
+				count1 ++;
+				newN = 1;
+				while (count1 < 2){
+				if (newN % 2 == 0){
+					newN = newN/2 ; 
+				}
+				else {
+					newN = ((n*3) + 1);	
+				}
+				//after each modification to newN add 1 to the counter
+				counter++ ;
+				//check to see if newN reached 1 again , if so add 1 to count1 to exit the loop
+				if (newN == 1){
+					count1 ++;
+				}
+				System.out.print(newN + " ");
+				}
+				//System.out.println(" (" + counter + ")");
+			
+			}
+			else {
+				while (i != 1){
+					
+					if (newN % 2 == 0){
+						newN = newN/2 ; 	
+					}
+					else {
+						newN = ((n*3) + 1);	
+					}
+
+					counter++ ;
+					System.out.print(newN + " ");
+				}
+			}
+			
+			System.out.println("(" + counter + ")");
+		}
+
 	}
-}
+		
+	}
+
